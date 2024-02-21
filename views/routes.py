d87b9387a1afff2400ed7ddf7145e7113ed698dc
Tristan Duncombe <32996075+tristanduncombe@users.noreply.github.com>
2024-02-21 01:32:34 +0000

    asdasdads

--- views/routes.py
+++ views/routes.py
@@ -35,7 +35,7 @@ def get_todo(id):
     return jsonify({
         "id": id,
         "title": "Watch CSSE6400 Lecture",
-        "description": "Watch CSSE6400 lecture on ECHO360 for week 1",
+        "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
         "completed": True,
         "deadline_at": "2023-02-27T00:00:00",
         "created_at": "2023-02-20T00:00:00",
